b7abae501ff4bf117963212773c403801493cb37
Avianna Yaptangco <yapavfee33@gmail.com>
2024-07-17 16:31:01 -0500

    Added Question 4 + 5

--- Final Project!!/Quiz.swift
+++ Final Project!!/Quiz.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct Quiz: View {
     @State private var placeholder = ""
-    var body: some View {
+    @State private var SpaceSea = ""
+    @State private var Organization = ""
+    var body: some View { ScrollView {
+    
         VStack{
             
+            //Question 1
             Text("Are you an extrovert or an introvert?")
                 .fontWeight(.black)
                 .padding(.top)
@@ -19,7 +23,7 @@ struct Quiz: View {
                 .foregroundColor(Color(hue: 0.927, saturation: 0.621, brightness: 1.0))
                 .font(.title2)
                 .multilineTextAlignment(.center)
-       
+            
             
             
             Button("Extrovert") {
@@ -50,6 +54,55 @@ struct Quiz: View {
                 .font(.system(size:50))
             
             
+            //Question 4
+            VStack {
+                Text("Do you prefer Space or the Sea?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.pink)
+                Button("Space") {
+                    SpaceSea = "🪐"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.pink)
+                
+                Button("Sea") {
+                    SpaceSea = "🌊"
+                }
+                .frame(width: 100.0, height: 100.0)
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+              
+               
+            }
+            Text(SpaceSea)
+                .font(.system(size: 100))
+            
+            VStack {
+                Text("When working on a project do you prefer heavy organization or adaptability?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.pink)
+                    .multilineTextAlignment(.center)
+                Button("Heavy planning and organization") {
+                    Organization = "🌊"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.pink)
+                
+                Button("Adaptabilty") {
+                    Organization = "🪐"
+                }
+                .frame(height: 100.0)
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+              
+               
+            }
+            Text(Organization)
+                .font(.system(size: 100))
+            
+        }
             
         }
         
