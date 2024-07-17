d42e587d1cb7df40b412282d2c030b19a459c08a
Avianna Yaptangco <yapavfee33@gmail.com>
2024-07-16 21:34:56 -0500

    put in buttons and connected other pages

---
+++ Final Project!!/FunFacts.swift
@@ -0,0 +1,18 @@
+//
+//  FunFacts.swift
+//  Final Project!!
+//
+//  Created by Vaneeka yaptangco on 7/16/24.
+//
+
+import SwiftUI
+
+struct FunFacts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FunFacts()
+}
---
+++ Final Project!!/ResourcesPage.swift
@@ -0,0 +1,18 @@
+//
+//  ResourcesPage.swift
+//  Final Project!!
+//
+//  Created by Vaneeka yaptangco on 7/16/24.
+//
+
+import SwiftUI
+
+struct ResourcesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResourcesPage()
+}
